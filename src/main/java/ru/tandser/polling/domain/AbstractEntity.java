785cda9ebd246206f7498318ebd3f790a17ebdd5
Andrew Timokhin <tandser@gmail.com>
2017-02-22 17:16:42 +0700

    Initial commit

---
+++ src/main/java/ru/tandser/polling/domain/AbstractEntity.java
@@ -0,0 +1,71 @@
+package ru.tandser.polling.domain;
+
+import com.google.common.base.MoreObjects;
+import org.hibernate.Hibernate;
+import org.springframework.data.domain.Persistable;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@MappedSuperclass
+public abstract class AbstractEntity implements Persistable<Integer> {
+
+    private Integer id;
+    private int     version;
+
+    @Override
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Version
+    @Column(name = "version")
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    @Override
+    @Transient
+    public boolean isNew() {
+        return getId() == null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || !getClass().equals(Hibernate.getClass(obj))) {
+            return false;
+        }
+
+        AbstractEntity that = (AbstractEntity) obj;
+
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId() != null ? getId() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id",      getId())
+                .add("version", getVersion())
+                .toString();
+    }
+}
