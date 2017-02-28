22f2d49de8bc94416e5f80ad68fb5cf89412fd8a
Andrew Timokhin <tandser@gmail.com>
2017-02-28 11:58:19 +0700

    Add ExceptionHandler

--- src/main/java/ru/tandser/polling/web/exc/RestExceptionHandler.java
+++ src/main/java/ru/tandser/polling/web/exc/RestExceptionHandler.java
@@ -3,14 +3,17 @@ package ru.tandser.polling.web.exc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.web.bind.annotation.*;
 import ru.tandser.polling.service.exc.NotFoundException;
 import ru.tandser.polling.web.Principal;
 
 import javax.servlet.http.HttpServletRequest;
 
+import static org.slf4j.event.Level.ERROR;
 import static org.slf4j.event.Level.WARN;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice(annotations = RestController.class)
 public class RestExceptionHandler {
@@ -34,6 +37,34 @@ public class RestExceptionHandler {
     @ResponseBody
     public ErrorInfo catchException(HttpServletRequest req, NotFoundException exc) {
         return logAndGetErrorInfo(req, exc, WARN);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(BAD_REQUEST)
+    @ResponseBody
+    public ErrorInfo catchException(HttpServletRequest req, BadRequestException exc) {
+        return logAndGetErrorInfo(req, exc, WARN);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ResponseStatus(CONFLICT)
+    @ResponseBody
+    public ErrorInfo catchException(HttpServletRequest req, DataIntegrityViolationException exc) {
+        return logAndGetErrorInfo(req, exc, WARN);
+    }
+
+    @ExceptionHandler(ObjectOptimisticLockingFailureException.class)
+    @ResponseStatus(CONFLICT)
+    @ResponseBody
+    public ErrorInfo catchException(HttpServletRequest req, ObjectOptimisticLockingFailureException exc) {
+        return logAndGetErrorInfo(req, exc, WARN);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorInfo catchException(HttpServletRequest req, Exception exc) {
+        return logAndGetErrorInfo(req, exc, ERROR);
     }
 
     private ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception exc, Level level) {
